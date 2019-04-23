ac2417d80e8e0d62219e0bd2b35a9bc9b233078f
Martine Danda <martine.danda@gmail.com>
2019-04-23 10:27:53 +0200

    FtS: Demo
    This change adds a cli demo for finding the shade.
    
    The demo consists from setting up the real photo color of
    face (the wanted color) and the photo color of the area of
    skin where foundation is applied. Then it names the found-
    ation, and lets the shade finder sort all known shades by
    match to the wanted color.
    
    It is notable that in this case, there is no good solution,
    because all closest shades have darker luminosity. The
    only one shade that is lighter than the target color is
    much brighter (per the luminosity difference) and thus
    won't be of much help either.
    
    UI and automated interpretation of results are for some
    other chapter.a
    
    In addition to the demo.c, there is also shell script
    with gcc one liner that compiles the demo.

---
+++ source/buildthedemo.sh
@@ -0,0 +1,1 @@
+gcc -I./findtheshade -L./findtheshade findtheshade/shades.c findtheshade/colorutils.c findtheshade/shadefinder.c demo.c -o findtheshadedemo -lm
---
+++ source/demo.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+
+#include <shadefinder.h>
+
+int main(int argc, char** argv) {
+    // Obtain the shade palette
+    rgb_shade* foundation_colors = all_foundation_colors();
+    int foundation_colors_size = all_foundation_colors_size();
+
+    // Define some input
+    rgb_color having = {230, 174, 159};
+    rgb_color wanted = {225, 182, 163};
+    const char* shade_name = "N4";
+
+    // Make the query
+    rgb_shade_search_result_with_meta search_results = closest_rgb_shades(
+        foundation_colors,
+        foundation_colors_size,
+        having,
+        shade_name,
+        wanted
+    );
+
+    // Process the results
+    if(search_results.result_arr && search_results.length) {
+        printf("Face color with the owned shade:\n ");
+        print_rgb_color(stdout, having);
+        printf("\n\nOwned shade:\n ");
+        print_rgb_shade(stdout, search_results.owned_shade);
+        printf("\n\n");
+        printf("Wanted face color:\n ");
+        print_rgb_color(stdout, wanted);
+        printf("\n\nProjected wanted shade color:\n ");
+        print_rgb_color(stdout, search_results.rgb_shade_wanted);
+        printf("; luminance_cmp: %.3f\n\nOptions:\n", search_results.wanted_luminance_cmp);
+
+        for(int i=0; i<search_results.length; i++) {
+            rgb_shade_search_result* sr = &search_results.result_arr[i];
+            printf(" - ");
+            print_rgb_shade(stdout, *(sr->mc));
+            printf(": (dist: %.3f); luminance_cmp: %.3f\n", sr->dist, sr->luminance_cmp);
+        }
+        free(search_results.result_arr);
+    }
+    else {
+        printf("No results for this query.\n");
+    }
+
+    return 0;
+}
