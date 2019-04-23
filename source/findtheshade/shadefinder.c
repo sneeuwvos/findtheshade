799f2215d6d0fd05bbfb96307bf4c6eaa60549ec
Martine Danda <martine.danda@gmail.com>
2019-04-23 10:25:07 +0200

    FtS: First Shade Finder
    This change adds a simple shade finder, that takes real life
    colors (wanted, having), and what shade was used. Then it
    sorts the known shades by closeness to the wanted color, and
    also returns metadata that helps interpret the result fur-
    ther:
    
      search_results, length
    
        An array of search results and its length. Each of array's
        elements contains:
    
          distance to the wanted color (dist)
          pointer to the shade (mc)
          luminance differnce to the wanted color (luminance_cmp)
    
      wanted_luminance_cmp
    
        Comparison of the wanted color's luminance with the used
        shade's. If it is greater than zero, it means that the
        wanted color is brighter than the used shade. And vice
        versa. When this value is zero, then the wanted and cur-
        rent color have same luminance but they could be very dif-
        ferent as colors.
    
      rgb_shade_wanted
    
        The projected wanted shade. It is generated by comparing
        the acquired shade color (real photo) with its ideal col-
        or as stored in shade.c, see
    
          04addd7cca401f498c61d37febf9ef42e7440c25
    
        There are many ways to do this. Currently, a very simple
        helpers are used, see transformation helpers in
    
          8246cd761cc4832c0638ac538febefd9a2f18e89
    
      owned_shade
    
        Link to the identified shade, per given name. If this
        shade is not found, the matcher cannot work.

---
+++ source/findtheshade/shadefinder.c
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+
+#include <shadefinder.h>
+
+int compare_temp_results_by_dist(const void *a, const void *b) {
+    return (int)(1000*((const rgb_shade_search_result*)a)->dist) - (int)(1000*((const rgb_shade_search_result*)b)->dist);
+}
+
+rgb_shade_search_result_with_meta closest_rgb_shades(
+    rgb_shade* rgb_shade_arr,
+    int rgb_shade_arr_size,
+
+    rgb_color color_having,
+    const char* color_having_name,
+    rgb_color color_wanted
+) {
+    // First obtain the color that we are having by name
+    rgb_shade rgb_shade_having = find_shade_by_name(
+        rgb_shade_arr,
+        rgb_shade_arr_size,
+        color_having_name
+    );
+
+    if(is_null_shade(rgb_shade_having)) {
+        rgb_shade_search_result_with_meta null_result;
+        return null_result;
+    } // TODO: any error message?
+
+    /**
+     * Now, obtain the transformation from the ideal color
+     * palette item to the color that is stated as 'having'.
+     *
+     * This will allow to translate the wanted color to the
+     * palette that this code knows about.
+     * */
+    double transf[3];
+    obtain_transformation_linear(
+        color_having, // source
+        rgb_shade_having.color, // target,
+        transf
+    );
+
+    /** Here it is. The wanted color */
+    rgb_color rgb_shade_wanted = apply_transformation_linear(color_wanted, transf);
+
+    // Compute its luminance diff ftr
+    double wanted_luminance_cmp = luminance_cmp(rgb_shade_wanted, rgb_shade_having.color);
+
+    /**
+     * This will now walk through all possible shades in
+     * search for best matches to achieve the wanted color.
+     * */
+    rgb_shade_search_result* search_results = calloc(rgb_shade_arr_size, sizeof(rgb_shade_search_result));
+
+    for(int i=0; i<rgb_shade_arr_size; i++) {
+        double dist = color_distance(rgb_shade_arr[i].color, rgb_shade_wanted);
+        int luminance_cmp_res = luminance_cmp(rgb_shade_arr[i].color, rgb_shade_wanted);
+        search_results[i].dist = dist;
+        search_results[i].mc = &rgb_shade_arr[i];
+        search_results[i].luminance_cmp = luminance_cmp_res;
+    }
+
+    qsort(search_results, rgb_shade_arr_size, sizeof(rgb_shade_search_result), compare_temp_results_by_dist);
+
+    rgb_shade_search_result_with_meta search_results_with_meta;
+
+    search_results_with_meta.result_arr = search_results;
+    search_results_with_meta.length = rgb_shade_arr_size;
+    search_results_with_meta.wanted_luminance_cmp = wanted_luminance_cmp;
+    search_results_with_meta.rgb_shade_wanted = rgb_shade_wanted;
+    search_results_with_meta.owned_shade = rgb_shade_having;
+
+    return search_results_with_meta;
+}
+
---
+++ source/findtheshade/shadefinder.h
@@ -0,0 +1,66 @@
+#ifndef INCL_FTS_SHADE_FINDER
+#define INCL_FTS_SHADE_FINDER
+
+#include <shades.h>
+#include <colorutils.h>
+
+/**
+ * Type for a search result
+ * */
+
+typedef struct {
+    double dist;
+    rgb_shade* mc;
+    double luminance_cmp;
+} rgb_shade_search_result;
+
+/**
+ * Wrapper for search result array, that also contains its
+ * length, and possibly other metadata in future.
+ * */
+
+typedef struct {
+    rgb_shade_search_result* result_arr;
+    int length;
+    double wanted_luminance_cmp;
+    rgb_color rgb_shade_wanted;
+    rgb_shade owned_shade;
+} rgb_shade_search_result_with_meta;
+
+/**
+ * Helper that determines the closest color matches, given
+ * all the colors
+ * */
+
+rgb_shade_search_result_with_meta closest_rgb_shades(
+    /**
+     * Array of rgb_shades that represent the assortment to
+     * choose from. Then also its length.
+     * */
+    rgb_shade* rgb_shade_arr,
+    int rgb_shade_arr_size,
+
+    /**
+     * The color one has got on their photo, by applying the
+     * make-up shade they have available
+     * */
+    rgb_color color_having,
+    /**
+     * Name of the make-up shade applied. This name has to be
+     * one of the names in the rgb_shade_arr array
+     * */
+    const char* color_having_name,
+    /**
+     * The color the user would like to achieve with a dif-
+     * ferent shade.
+     * */
+    rgb_color color_wanted
+);
+
+/**
+ * Result comparator by dist
+ * */
+
+int compare_temp_results_by_dist(const void *, const void *); // Wants two of type rgb_shade_search_result
+
+#endif
