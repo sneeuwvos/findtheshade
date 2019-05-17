38a0a3eab68eab796b49ca08b69d924a230d3136
Martine Danda <martine.danda@gmail.com>
2019-05-17 20:57:38 +0200

    FtS: Compile Process += -Wpedantic -ansi
    This change appends the compile instruction to also warn of
    -Wpedantic spots and -ansi incompatibility.

--- source/buildthedemo.sh
+++ source/buildthedemo.sh
@@ -1,1 +1,1 @@
-gcc -I./findtheshade -L./findtheshade findtheshade/shades.c findtheshade/colorutils.c findtheshade/shadefinder.c findtheshade/affinetransf.c demo.c -o findtheshadedemo -lm -Wall -Wextra -std=c99
+gcc -I./findtheshade -L./findtheshade findtheshade/shades.c findtheshade/colorutils.c findtheshade/shadefinder.c findtheshade/affinetransf.c demo.c -o findtheshadedemo -lm -Wall -Wextra -std=c99 -Wpedantic -ansi
